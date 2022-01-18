27ecfe9778b88ade53f977f31d9c37c3467d7d83
chris <christorper@gmail.com>
2022-01-18 13:19:33 -0800

    Add wrapper to buttons and styling

--- src/makeMenu.js
+++ src/makeMenu.js
@@ -1,36 +1,46 @@
-import emojis from "./images/emoji_symbols_black_24dp.svg";
-import frontHand from "./images/front_hand_black_24dp.svg";
-import luggage from "./images/luggage_black_24dp.svg";
-import piano from "./images/piano_black_24dp.svg";
-import skateboard from "./images/skateboarding_black_24dp.svg";
+import emojis from './images/emoji_symbols_black_24dp.svg';
+import frontHand from './images/front_hand_black_24dp.svg';
+import luggage from './images/luggage_black_24dp.svg';
+import piano from './images/piano_black_24dp.svg';
+import skateboard from './images/skateboarding_black_24dp.svg';
+import menu from './images/menu_black_24dp.svg';
 
 const makeMenu = () => {
-  const body = document.querySelector("body");
+  const body = document.querySelector('body');
   const title = document.createElement('div');
-  title.textContent = "A nice menu";
+  const menuIcon = new Image();
+  menuIcon.src = menu;
+  const titleText = document.createElement('div');
+  titleText.textContent = 'A nice menu';
+  title.appendChild(menuIcon);
+  title.appendChild(titleText);
   title.classList.add('title');
-  const nav = document.createElement("ul");
+  const nav = document.createElement('ul');
   nav.appendChild(title);
-  nav.classList.add("nav");
+  nav.classList.add('nav');
+  const wrapper = document.createElement('ul');
+  wrapper.classList.add('wrapper');
+  nav.appendChild(wrapper);
 
   const menuItems = [emojis, frontHand, luggage, piano, skateboard];
 
   menuItems.forEach((item) => {
-    const tab = document.createElement("li");
-    tab.classList.add("tab");
-    const innerLink = document.createElement("a");
-    innerLink.classList.add("inner-link");
-    innerLink.href = "#";
+    const tab = document.createElement('li');
+    tab.classList.add('tab');
+    const innerLink = document.createElement('a');
+    innerLink.classList.add('inner-link');
+    innerLink.href = '#';
     const materialIcon = new Image();
     materialIcon.src = item;
     innerLink.appendChild(materialIcon);
     tab.appendChild(innerLink);
-    nav.appendChild(tab);
+    wrapper.appendChild(tab);
   });
 
-  nav.addEventListener("click", (e) => {
-    if (e.target.className === "inner-link") {
-      e.target.classList.toggle("selected");
+  nav.addEventListener('click', (e) => {
+    if (e.target.className === 'inner-link') {
+      
+      e.target.classList.toggle('selected');
     }
   });
 
